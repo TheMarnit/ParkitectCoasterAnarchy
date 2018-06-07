c487050672f0a781bf5e70b357160c9e503628ed
Marnit <githubmarnit@gmail.com>
2018-06-07 02:21:55 +0200

    Code changes, preview

--- Main.cs
+++ Main.cs
@@ -11,8 +11,57 @@ namespace CoasterAnarchy
         public void onEnabled()
         {
             GameObject lsmFin = TrackRideHelper.GetTrackedRide("Floorless Coaster").meshGenerator.lsmFinGO;
-            TrackRideHelper.GetTrackedRide("Wooden Coaster").canHaveLSM = true;
-            TrackRideHelper.GetTrackedRide("Wooden Coaster").meshGenerator.lsmFinGO = lsmFin;
+            SpecialSegmentsList specials = TrackRideHelper.GetTrackedRide("Floorless Coaster").specialSegments;
+            CoasterCarInstantiator[] carTypes = { };
+            foreach (Attraction current in ScriptableSingleton<AssetManager>.Instance.getAttractionObjects())
+            {
+                var ride = current as TrackedRide;
+                if (ride != null)
+                {
+                    carTypes = carTypes.Union(ride.carTypes).ToArray();
+                }
+            }
+            foreach (Attraction current in ScriptableSingleton<AssetManager>.Instance.getAttractionObjects())
+            {
+                var ride = current as TrackedRide;
+                if (ride != null)
+                {
+                    ride.canAdjustLiftSpeeds = true;
+                    ride.canHaveLSM = true;
+                    ride.canChangeLaps = true;
+                    ride.canCurveLifts = true;
+                    ride.canCurveSlopes = true;
+                    ride.canCurveVertical = true;
+                    if (ride.getUnlocalizedName().Contains("Coaster"))
+                    {
+                        ride.canHaveBrakes = true;
+                    }
+                    ride.canHaveHoldingBrakes = true;
+                    if (ride.getUnlocalizedName().Contains("Coaster") || ride.everyUpIsLift)
+                    {
+                        ride.canHaveLifts = true;
+                    }
+                    ride.canInvertSlopes = true;
+                    ride.canChangeCarRotation = true;
+                    ride.everyUpIsLift = false;
+                    ride.maxBankingAngle = 180;
+                    ride.maxDeltaHeightDown = float.MaxValue;
+                    ride.maxDeltaHeightForLift = float.MaxValue;
+                    ride.maxDeltaHeightPerUnit = float.MaxValue;
+                    ride.maxDeltaHeightUp = float.MaxValue;
+                    ride.maxLiftSpeed = float.MaxValue;
+                    ride.maxSupportHeight = int.MaxValue;
+                    ride.meshGenerator.lsmFinGO = lsmFin;
+                    ride.min90CurveSize = 1;
+                    ride.minHalfHelixSize = 1;
+                    if (ride.getUnlocalizedName() != "Hydraulically-Launched Coaster")
+                    {
+                        ride.specialSegments = specials;
+                    }
+                    ride.carTypes = ride.carTypes.Union(carTypes).ToArray();
+                }
+
+            }
         }
 
         public void onDisabled()
