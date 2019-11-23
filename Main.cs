f3ba2e0688852475de9bbbdca2e5d1dd01fe0de9
marnit <martin-van-wijk-1990@hotmail.com>
2019-11-23 03:59:01 +0100

    disable now works

--- Main.cs
+++ Main.cs
@@ -7,9 +7,13 @@ namespace CoasterAnarchy
 {
     public class Main : IMod
     {
+        Dictionary<string, TrackedRide> originalSettings = new Dictionary<string, TrackedRide>();
+        Dictionary<string, List<SpecialSegmentSettings>> originalSegments = new Dictionary<string, List<SpecialSegmentSettings>>();
 
         public void onEnabled()
         {
+            originalSettings = new Dictionary<string, TrackedRide>();
+            originalSegments = new Dictionary<string, List<SpecialSegmentSettings>>();
             Global.NO_TRACKBUILDER_RESTRICTIONS = true;
             GameObject lsmFin = TrackRideHelper.GetTrackedRide("Floorless Coaster").meshGenerator.lsmFinGO;
             SpecialSegmentsList specials = TrackRideHelper.GetTrackedRide("Steel Coaster").specialSegments;
@@ -25,50 +29,80 @@ namespace CoasterAnarchy
             foreach (Attraction current in ScriptableSingleton<AssetManager>.Instance.getAttractionObjects())
             {
                 var ride = current as TrackedRide;
+                TrackedRide originalRide = new TrackedRide();
                 if (ride != null)
                 {
+                    originalRide.canAdjustLiftSpeeds = ride.canAdjustLiftSpeeds;
                     ride.canAdjustLiftSpeeds = true;
+                    originalRide.canHaveLSM = ride.canHaveLSM;
                     ride.canHaveLSM = true;
+                    originalRide.canChangeLaps = ride.canChangeLaps;
                     ride.canChangeLaps = true;
+                    originalRide.canCurveLifts = ride.canCurveLifts;
                     ride.canCurveLifts = true;
+                    originalRide.canCurveSlopes = ride.canCurveSlopes;
                     ride.canCurveSlopes = true;
+                    originalRide.canCurveVertical = ride.canCurveVertical;
                     ride.canCurveVertical = true;
+                    originalRide.canHaveBrakes = ride.canHaveBrakes;
+                    originalRide.canHaveBlockBrakes = ride.canHaveBlockBrakes;
                     if (ride.getUnlocalizedName().Contains("Coaster"))
                     {
                         ride.canHaveBrakes = true;
                         ride.canHaveBlockBrakes = true;
                     }
+                    originalRide.canHaveHoldingBrakes = ride.canHaveHoldingBrakes;
                     ride.canHaveHoldingBrakes = true;
+                    originalRide.canHaveLifts = ride.canHaveLifts;
                     if (ride.getUnlocalizedName().Contains("Coaster") || ride.everyUpIsLift)
                     {
                         ride.canHaveLifts = true;
                     }
+                    originalRide.canInvertSlopes = ride.canInvertSlopes;
                     ride.canInvertSlopes = true;
+                    originalRide.canChangeCarRotation = ride.canChangeCarRotation;
                     ride.canChangeCarRotation = true;
+                    originalRide.everyUpIsLift = ride.everyUpIsLift;
                     ride.everyUpIsLift = false;
+                    originalRide.maxBankingAngle = ride.maxBankingAngle;
                     ride.maxBankingAngle = 180;
+                    originalRide.maxDeltaHeightDown = ride.maxDeltaHeightDown;
                     ride.maxDeltaHeightDown = float.MaxValue;
+                    originalRide.maxDeltaHeightForLift = ride.maxDeltaHeightForLift;
                     ride.maxDeltaHeightForLift = float.MaxValue;
+                    originalRide.maxDeltaHeightPerUnit = ride.maxDeltaHeightPerUnit;
                     ride.maxDeltaHeightPerUnit = float.MaxValue;
+                    originalRide.maxDeltaHeightUp = ride.maxDeltaHeightUp;
                     ride.maxDeltaHeightUp = float.MaxValue;
+                    originalRide.maxLiftSpeed = ride.maxLiftSpeed;
                     ride.maxLiftSpeed = 100f;
+                    originalRide.maxSupportHeight = ride.maxSupportHeight;
                     ride.maxSupportHeight = int.MaxValue;
-                    ride.meshGenerator.lsmFinGO = lsmFin;
+                    if (ride.meshGenerator.lsmFinGO == null)
+                    {
+                        ride.meshGenerator.lsmFinGO = lsmFin;
+                    }
+                    originalRide.min90CurveSize = ride.min90CurveSize;
                     ride.min90CurveSize = 1;
+                    originalRide.minHalfHelixSize = ride.minHalfHelixSize;
                     ride.minHalfHelixSize = 1;
+                    List<SpecialSegmentSettings> originalSpecialSegments = new List<SpecialSegmentSettings>();
                     foreach (SpecialSegmentSettings segment in ScriptableSingleton<AssetManager>.Instance.specialSegments)
                     {
                         Debug.Log(segment.name);
-                        if (segment.segmentPrefab.GetType() == typeof(HalfLoopUp))
+                        if(ride.specialSegments.hasSpecialSegment(segment))
                         {
+                            originalSpecialSegments.Add(segment);
                         }
-                        Debug.Log(segment.segmentPrefab.canControlBanking());
                         if (segment.name != "Splashdown" && segment.name != "HydraulicLaunchSystem")
                         {
                             ride.specialSegments.addSpecialSegment(segment);
                         }
                     }
+                    originalRide.carTypes = ride.carTypes;
                     ride.carTypes = ride.carTypes.Union(carTypes).ToArray();
+                    originalSettings.Add(ride.getUnlocalizedName(), originalRide);
+                    originalSegments.Add(ride.getUnlocalizedName(), originalSpecialSegments);
                 }
 
             }
@@ -76,7 +110,45 @@ namespace CoasterAnarchy
 
         public void onDisabled()
         {
-
+            Global.NO_TRACKBUILDER_RESTRICTIONS = false;
+            foreach (Attraction current in ScriptableSingleton<AssetManager>.Instance.getAttractionObjects())
+            {
+                var ride = current as TrackedRide;
+                if (ride != null && originalSettings.ContainsKey(ride.getUnlocalizedName()))
+                {
+                    ride.canAdjustLiftSpeeds = originalSettings[ride.getUnlocalizedName()].canAdjustLiftSpeeds;
+                    ride.canHaveLSM = originalSettings[ride.getUnlocalizedName()].canHaveLSM;
+                    ride.canChangeLaps = originalSettings[ride.getUnlocalizedName()].canChangeLaps;
+                    ride.canCurveLifts = originalSettings[ride.getUnlocalizedName()].canCurveLifts;
+                    ride.canCurveSlopes = originalSettings[ride.getUnlocalizedName()].canCurveSlopes;
+                    ride.canCurveVertical = originalSettings[ride.getUnlocalizedName()].canCurveVertical;
+                    ride.canHaveBrakes = originalSettings[ride.getUnlocalizedName()].canHaveBrakes;
+                    ride.canHaveBlockBrakes = originalSettings[ride.getUnlocalizedName()].canHaveBlockBrakes;
+                    ride.canHaveHoldingBrakes = originalSettings[ride.getUnlocalizedName()].canHaveHoldingBrakes;
+                    ride.canHaveLifts = originalSettings[ride.getUnlocalizedName()].canHaveLifts;
+                    ride.canInvertSlopes = originalSettings[ride.getUnlocalizedName()].canInvertSlopes;
+                    ride.canChangeCarRotation = originalSettings[ride.getUnlocalizedName()].canChangeCarRotation;
+                    ride.everyUpIsLift = originalSettings[ride.getUnlocalizedName()].everyUpIsLift;
+                    ride.maxBankingAngle = originalSettings[ride.getUnlocalizedName()].maxBankingAngle;
+                    ride.maxDeltaHeightDown = originalSettings[ride.getUnlocalizedName()].maxDeltaHeightDown;
+                    ride.maxDeltaHeightForLift = originalSettings[ride.getUnlocalizedName()].maxDeltaHeightForLift;
+                    ride.maxDeltaHeightPerUnit = originalSettings[ride.getUnlocalizedName()].maxDeltaHeightPerUnit;
+                    ride.maxDeltaHeightUp = originalSettings[ride.getUnlocalizedName()].maxDeltaHeightUp;
+                    ride.maxLiftSpeed = originalSettings[ride.getUnlocalizedName()].maxLiftSpeed;
+                    ride.maxSupportHeight = originalSettings[ride.getUnlocalizedName()].maxSupportHeight;
+                    ride.min90CurveSize = originalSettings[ride.getUnlocalizedName()].min90CurveSize;
+                    ride.minHalfHelixSize = originalSettings[ride.getUnlocalizedName()].minHalfHelixSize;
+                    ride.carTypes = originalSettings[ride.getUnlocalizedName()].carTypes;
+                    foreach (SpecialSegmentSettings segment in ScriptableSingleton<AssetManager>.Instance.specialSegments)
+                    {
+                        ride.specialSegments.removeSpecialSegment(segment);
+                    }
+                    foreach (SpecialSegmentSettings segment in originalSegments[ride.getUnlocalizedName()])
+                    {
+                        ride.specialSegments.addSpecialSegment(segment);
+                    }
+                }
+            }
         }
 
         public string Name => "Coaster Anarchy";
